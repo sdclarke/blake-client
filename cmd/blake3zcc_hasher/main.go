13cdeb9c626f7043ec5dd2276631a0c30645ea6b
Scott Clarke <sdclarke123@gmail.com>
2020-11-18 12:22:09 +0000

    Add code to hash and upload directories
    The client is now capable of hashing a directory and uploading it to the
    CAS using either BLAKE3ZCC or SHA256. It will only upload missing blobs
    from the directory. It will create batches of FindMissingBlobs calls
    before uploading in order to reduce the number of calls made to the
    server.

--- cmd/blake3zcc_hasher/main.go
+++ cmd/blake3zcc_hasher/main.go
@@ -3,16 +3,12 @@ package main
 import (
 	"context"
 	"crypto/sha256"
-	"encoding/hex"
 	"flag"
-	"fmt"
-	"io"
 	"log"
-	"os"
 
-	remoteexecution "github.com/bazelbuild/remote-apis/build/bazel/remote/execution/v2"
+	//remoteexecution "github.com/bazelbuild/remote-apis/build/bazel/remote/execution/v2"
 	"github.com/buildbarn/bb-storage/pkg/digest"
-	bpb "google.golang.org/genproto/googleapis/bytestream"
+	"github.com/sdclarke/blake-client/pkg/blobuploader"
 	"google.golang.org/grpc"
 )
 
@@ -40,11 +36,10 @@ func main() {
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
 	}
-	casClient := remoteexecution.NewContentAddressableStorageClient(conn)
+	//casClient := remoteexecution.NewContentAddressableStorageClient(conn)
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	blobUploader := blobUploader.NewBlobUploader(conn, 100)
 	instanceName, err := digest.NewInstanceName("blake-client")
 	if err != nil {
 		log.Fatalf("Error creating instance name: %v", err)
@@ -60,88 +55,15 @@ func main() {
 		}
 		hash = d.NewHasher()
 	}
-	file, err := os.Open("/home/scott/buildbarn/bb-storage/randomfile")
+	blobUploader, finaliser := blobuploader.NewBlobUploader(conn, instanceName.String(), 100, hash, blake)
+
+	inputRootDigest, err := blobUploader.UploadDirectory(ctx, inputRoot)
 	if err != nil {
-		log.Fatalf("Failed to open file: %v", err)
+		log.Fatalf("Something went wrong uploading input root: %v", err)
 	}
-	defer file.Close()
-
-	stat, err := file.Stat()
+	err = finaliser(ctx)
 	if err != nil {
-		log.Fatalf("%v", err)
+		log.Fatalf("Something went wrong uploading input root: %v", err)
 	}
-
-	b := make([]byte, stat.Size())
-	_, err = file.Read(b)
-	if err != nil {
-		log.Fatalf("Failed to read file: %v", err)
-	}
-	_, err = file.Seek(0, 0)
-	if err != nil {
-		log.Fatalf("%v", err)
-	}
-
-	if _, err := io.Copy(hash, file); err != nil {
-		log.Fatalf("%v", err)
-	}
-
-	var digest digest.Digest
-	if blake {
-		digest, err = instanceName.NewDigest(fmt.Sprintf("B3Z:%s", hex.EncodeToString(hash.Sum(nil))), stat.Size())
-	} else {
-		digest, err = instanceName.NewDigest(hex.EncodeToString(hash.Sum(nil)), stat.Size())
-	}
-	if err != nil {
-		log.Fatalf("Error creating digest for file: %v", err)
-	}
-	//log.Printf("Digest %v", digest)
-
-	findMissingRequest := &remoteexecution.FindMissingBlobsRequest{
-		InstanceName: instanceName.String(),
-		BlobDigests:  []*remoteexecution.Digest{digest.GetProto()},
-	}
-
-	findMissingResponse, err := casClient.FindMissingBlobs(ctx, findMissingRequest)
-	if err != nil {
-		log.Fatalf("Error with FindMissingBlobs: %v", err)
-	}
-	//log.Printf("Find Missing Response: %v", findMissingResponse)
-
-	if len(findMissingResponse.MissingBlobDigests) != 0 {
-		updateRequest := &remoteexecution.BatchUpdateBlobsRequest{
-			InstanceName: instanceName.String(),
-			Requests: []*remoteexecution.BatchUpdateBlobsRequest_Request{{
-				Digest: digest.GetProto(),
-				Data:   b,
-			}},
-		}
-
-		_, err = casClient.BatchUpdateBlobs(ctx, updateRequest)
-		if err != nil {
-			log.Fatalf("Error with BatchUpdateBlobs: %v", err)
-		}
-	}
-
-	//log.Printf("Response %v", updateResponse)
-
-	//readResponse, err := casClient.BatchReadBlobs(ctx, &remoteexecution.BatchReadBlobsRequest{
-	//InstanceName: "test",
-	//Digests:      []*remoteexecution.Digest{digest.GetProto()},
-	//})
-	//if err != nil {
-	//log.Fatalf("Error with BatchReadBlobs: %v", err)
-	//}
-
-	//newFile, err := os.Create("/home/scott/output.png")
-	//if err != nil {
-	//log.Fatalf("Failed to create output file: %v", err)
-	//}
-
-	//_, err = newFile.Write(readResponse.Responses[0].Data)
-	//if err != nil {
-	//log.Fatalf("Failed to write to output file: %v", err)
-	//}
-	//if manifestDigest, _, ok := digest.ToManifest(int64(8 * 1024)); ok {
-	//log.Printf("Manifest Size: %v", manifestDigest.GetSizeBytes())
-	//}
+	log.Printf("Input root digest: %v", inputRootDigest)
 }
