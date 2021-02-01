0b0e70cf345fb87d969eb218444c6dddce309348
Scott Clarke <sdclarke123@gmail.com>
2021-02-01 15:45:02 +0000

    Finish off scripts for experiments
    The experiments scripts have now been worked on more, making them
    complete for now

--- input_generation.sh
+++ input_generation.sh
@@ -6,11 +6,11 @@ do
     s) size=${OPTARG};;
   esac
 done
+rm -rf experiments
+mkdir -p experiments/e{1..12}
+cd experiments/e1
 case $size in 
   small)
-    rm -rf experiments
-    mkdir -p experiments/e{1..12}
-    cd experiments/e1
     dd if=/dev/zero of=in0 bs=512 count=2
     dd if=/dev/zero of=in1 bs=512 count=3
     cd ../e2
@@ -46,5 +46,190 @@ case $size in
     cd ../e12
     dd if=/dev/zero of=in0 bs=512 count=3
     dd if=/dev/zero of=in1 bs=512 count=2
+    ;;
+  small_multi)
+    dd if=/dev/zero of=in0 bs=$((1*8192+1)) count=1
+    dd if=/dev/zero of=in0 bs=$((2*8192+1)) count=1
+    cd ../e2
+    dd if=/dev/zero of=in0 bs=$((1*8192+1)) count=1
+    dd if=/dev/zero of=in0 bs=$((4*8192+1)) count=1
+    cd ../e3
+    dd if=/dev/zero of=in0 bs=$((1*8192+1)) count=1
+    dd if=/dev/zero of=in0 bs=$((5*8192-1)) count=1
+    cd ../e4
+    dd if=/dev/zero of=in0 bs=$((2*8192+1)) count=1
+    dd if=/dev/zero of=in0 bs=$((4*8192+1)) count=1
+    cd ../e5
+    dd if=/dev/zero of=in0 bs=$((2*8192+1)) count=1
+    dd if=/dev/zero of=in0 bs=$((5*8192-1)) count=1
+    cd ../e6
+    dd if=/dev/zero of=in0 bs=$((4*8192+1)) count=1
+    dd if=/dev/zero of=in0 bs=$((5*8192-1)) count=1
+    cd ../e7
+    dd if=/dev/zero of=in0 bs=$((5*8192-1)) count=1
+    dd if=/dev/zero of=in0 bs=$((4*8192+1)) count=1
+    cd ../e8
+    dd if=/dev/zero of=in0 bs=$((5*8192-1)) count=1
+    dd if=/dev/zero of=in0 bs=$((2*8192+1)) count=1
+    cd ../e9
+    dd if=/dev/zero of=in0 bs=$((5*8192-1)) count=1
+    dd if=/dev/zero of=in0 bs=$((1*8192+1)) count=1
+    cd ../e10
+    dd if=/dev/zero of=in0 bs=$((4*8192+1)) count=1
+    dd if=/dev/zero of=in0 bs=$((2*8192+1)) count=1
+    cd ../e11
+    dd if=/dev/zero of=in0 bs=$((4*8192+1)) count=1
+    dd if=/dev/zero of=in0 bs=$((1*8192+1)) count=1
+    cd ../e12
+    dd if=/dev/zero of=in0 bs=$((2*8192+1)) count=1
+    dd if=/dev/zero of=in0 bs=$((1*8192+1)) count=1
+    ;;
+  medium)
+    dd if=/dev/zero of=in0 bs=$((5*8192+1)) count=1
+    dd if=/dev/zero of=in1 bs=$((20*8192+1)) count=1
+    cd ../e2
+    dd if=/dev/zero of=in0 bs=$((5*8192+1)) count=1
+    dd if=/dev/zero of=in1 bs=$((35*8192+1)) count=1
+    cd ../e3
+    dd if=/dev/zero of=in0 bs=$((5*8192+1)) count=1
+    dd if=/dev/zero of=in1 bs=$((55*8192+1)) count=1
+    cd ../e4
+    dd if=/dev/zero of=in0 bs=$((20*8192+1)) count=1
+    dd if=/dev/zero of=in1 bs=$((35*8192+1)) count=1
+    cd ../e5
+    dd if=/dev/zero of=in0 bs=$((20*8192+1)) count=1
+    dd if=/dev/zero of=in1 bs=$((55*8192+1)) count=1
+    cd ../e6
+    dd if=/dev/zero of=in0 bs=$((35*8192+1)) count=1
+    dd if=/dev/zero of=in1 bs=$((55*8192+1)) count=1
+    cd ../e7
+    dd if=/dev/zero of=in0 bs=$((55*8192+1)) count=1
+    dd if=/dev/zero of=in1 bs=$((35*8192+1)) count=1
+    cd ../e8
+    dd if=/dev/zero of=in0 bs=$((55*8192+1)) count=1
+    dd if=/dev/zero of=in1 bs=$((20*8192+1)) count=1
+    cd ../e9
+    dd if=/dev/zero of=in0 bs=$((55*8192+1)) count=1
+    dd if=/dev/zero of=in1 bs=$((5*8192+1)) count=1
+    cd ../e10
+    dd if=/dev/zero of=in0 bs=$((35*8192+1)) count=1
+    dd if=/dev/zero of=in1 bs=$((20*8192+1)) count=1
+    cd ../e11
+    dd if=/dev/zero of=in0 bs=$((35*8192+1)) count=1
+    dd if=/dev/zero of=in1 bs=$((5*8192+1)) count=1
+    cd ../e12
+    dd if=/dev/zero of=in0 bs=$((20*8192+1)) count=1
+    dd if=/dev/zero of=in1 bs=$((5*8192+1)) count=1
+    ;;
+  medium_high)
+    dd if=/dev/zero of=in0 bs=$((65*8192+1)) count=1
+    dd if=/dev/zero of=in1 bs=$((107*8192+1)) count=1
+    cd ../e2
+    dd if=/dev/zero of=in0 bs=$((65*8192+1)) count=1
+    dd if=/dev/zero of=in1 bs=$((149*8192+1)) count=1
+    cd ../e3
+    dd if=/dev/zero of=in0 bs=$((65*8192+1)) count=1
+    dd if=/dev/zero of=in1 bs=$((191*8192+1)) count=1
+    cd ../e4
+    dd if=/dev/zero of=in0 bs=$((107*8192+1)) count=1
+    dd if=/dev/zero of=in1 bs=$((149*8192+1)) count=1
+    cd ../e5
+    dd if=/dev/zero of=in0 bs=$((107*8192+1)) count=1
+    dd if=/dev/zero of=in1 bs=$((191*8192+1)) count=1
+    cd ../e6
+    dd if=/dev/zero of=in0 bs=$((149*8192+1)) count=1
+    dd if=/dev/zero of=in1 bs=$((191*8192+1)) count=1
+    cd ../e7
+    dd if=/dev/zero of=in0 bs=$((191*8192+1)) count=1
+    dd if=/dev/zero of=in1 bs=$((149*8192+1)) count=1
+    cd ../e8
+    dd if=/dev/zero of=in0 bs=$((191*8192+1)) count=1
+    dd if=/dev/zero of=in1 bs=$((107*8192+1)) count=1
+    cd ../e9
+    dd if=/dev/zero of=in0 bs=$((191*8192+1)) count=1
+    dd if=/dev/zero of=in1 bs=$((65*8192+1)) count=1
+    cd ../e10
+    dd if=/dev/zero of=in0 bs=$((149*8192+1)) count=1
+    dd if=/dev/zero of=in1 bs=$((107*8192+1)) count=1
+    cd ../e11
+    dd if=/dev/zero of=in0 bs=$((149*8192+1)) count=1
+    dd if=/dev/zero of=in1 bs=$((65*8192+1)) count=1
+    cd ../e12
+    dd if=/dev/zero of=in0 bs=$((107*8192+1)) count=1
+    dd if=/dev/zero of=in1 bs=$((65*8192+1)) count=1
+    ;;
+  large)
+    dd if=/dev/zero of=in0 bs=$((2*1024*1024+1)) count=1
+    dd if=/dev/zero of=in1 bs=$((18*1024*1024+1)) count=1
+    cd ../e2
+    dd if=/dev/zero of=in0 bs=$((2*1024*1024+1)) count=1
+    dd if=/dev/zero of=in1 bs=$((34*1024*1024+1)) count=1
+    cd ../e3
+    dd if=/dev/zero of=in0 bs=$((2*1024*1024+1)) count=1
+    dd if=/dev/zero of=in1 bs=$((50*1024*1024)) count=1
+    cd ../e4
+    dd if=/dev/zero of=in0 bs=$((18*1024*1024+1)) count=1
+    dd if=/dev/zero of=in1 bs=$((34*1024*1024))  count=1
+    cd ../e5
+    dd if=/dev/zero of=in0 bs=$((18*1024*1024+1)) count=1
+    dd if=/dev/zero of=in1 bs=$((50*1024*1024))  count=1
+    cd ../e6
+    dd if=/dev/zero of=in0 bs=$((34*1024*1024+1)) count=1
+    dd if=/dev/zero of=in1 bs=$((50*1024*1024))  count=1
+    cd ../e7
+    dd if=/dev/zero of=in0 bs=$((50*1024*1024+1)) count=1
+    dd if=/dev/zero of=in1 bs=$((34*1024*1024))  count=1
+    cd ../e8
+    dd if=/dev/zero of=in0 bs=$((50*1024*1024+1)) count=1
+    dd if=/dev/zero of=in1 bs=$((18*1024*1024))  count=1
+    cd ../e9
+    dd if=/dev/zero of=in0 bs=$((50*1024*1024+1)) count=1
+    dd if=/dev/zero of=in1 bs=$((2*1024*1024)) count=1
+    cd ../e10
+    dd if=/dev/zero of=in0 bs=$((34*1024*1024+1)) count=1
+    dd if=/dev/zero of=in1 bs=$((18*1024*1024))  count=1
+    cd ../e11
+    dd if=/dev/zero of=in0 bs=$((34*1024*1024+1)) count=1
+    dd if=/dev/zero of=in1 bs=$((2*1024*1024)) count=1
+    cd ../e12
+    dd if=/dev/zero of=in0 bs=$((18*1024*1024+1)) count=1
+    dd if=/dev/zero of=in1 bs=$((2*1024*1024)) count=1
+    ;;
+  huge)
+    dd if=/dev/zero of=in0 bs=$((265*1024*1024+1)) count=1
+    dd if=/dev/zero of=in1 bs=$((275*1024*1024+1)) count=1
+    cd ../e2
+    dd if=/dev/zero of=in0 bs=$((265*1024*1024+1)) count=1
+    dd if=/dev/zero of=in1 bs=$((530*1024*1024+1)) count=1
+    cd ../e3
+    dd if=/dev/zero of=in0 bs=$((265*1024*1024+1)) count=1
+    dd if=/dev/zero of=in1 bs=$((540*1024*1024)) count=1
+    cd ../e4
+    dd if=/dev/zero of=in0 bs=$((275*1024*1024+1)) count=1
+    dd if=/dev/zero of=in1 bs=$((530*1024*1024))  count=1
+    cd ../e5
+    dd if=/dev/zero of=in0 bs=$((275*1024*1024+1)) count=1
+    dd if=/dev/zero of=in1 bs=$((540*1024*1024))  count=1
+    cd ../e6
+    dd if=/dev/zero of=in0 bs=$((530*1024*1024+1)) count=1
+    dd if=/dev/zero of=in1 bs=$((540*1024*1024))  count=1
+    cd ../e7
+    dd if=/dev/zero of=in0 bs=$((540*1024*1024+1)) count=1
+    dd if=/dev/zero of=in1 bs=$((530*1024*1024))  count=1
+    cd ../e8
+    dd if=/dev/zero of=in0 bs=$((540*1024*1024+1)) count=1
+    dd if=/dev/zero of=in1 bs=$((275*1024*1024))  count=1
+    cd ../e9
+    dd if=/dev/zero of=in0 bs=$((540*1024*1024+1)) count=1
+    dd if=/dev/zero of=in1 bs=$((265*1024*1024)) count=1
+    cd ../e10
+    dd if=/dev/zero of=in0 bs=$((530*1024*1024+1)) count=1
+    dd if=/dev/zero of=in1 bs=$((275*1024*1024))  count=1
+    cd ../e11
+    dd if=/dev/zero of=in0 bs=$((530*1024*1024+1)) count=1
+    dd if=/dev/zero of=in1 bs=$((265*1024*1024)) count=1
+    cd ../e12
+    dd if=/dev/zero of=in0 bs=$((275*1024*1024+1)) count=1
+    dd if=/dev/zero of=in1 bs=$((265*1024*1024)) count=1
     ;;
 esac
